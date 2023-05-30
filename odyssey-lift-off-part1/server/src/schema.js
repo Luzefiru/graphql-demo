f6df611fbcf6935cc2750628430c532229ae95c6
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-30 18:46:16 +0800

    feat: finish 'Building our schema' section

--- odyssey-lift-off-part1/server/src/schema.js
+++ odyssey-lift-off-part1/server/src/schema.js
@@ -1,6 +1,11 @@
 const gql = require('graphql-tag');
 
 const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
+
   "A track is a group of Modules that teach a specific topic"
   type Track {
     id: ID!
