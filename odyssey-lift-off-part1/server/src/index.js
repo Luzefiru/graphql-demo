916f0ed1bfb2650b895725c27e3d7b27ac7cfc76
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-30 19:01:42 +0800

    feat: add boilerplate for Apollo Server

--- odyssey-lift-off-part1/server/src/index.js
+++ odyssey-lift-off-part1/server/src/index.js
@@ -1,1 +1,11 @@
-//TODO
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const typeDefs = require('./schema.js');
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs }); // destructure typeDefs as args
+  const { url } = await startStandaloneServer(server); // await the URL
+  console.log(`Server is being served on ${url}`); // log it and query it later
+}
+
+startApolloServer();
