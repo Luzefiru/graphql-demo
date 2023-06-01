508553ed9cc2a7ac0ca640e8d88b61867a46a4dc
Luzefiru <ian.deejay.iv@gmail.com>
2023-06-01 15:22:34 +0800

    feat: add module endpoint

--- odyssey-lift-off-lab/server/src/schema.js
+++ odyssey-lift-off-lab/server/src/schema.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track!
+    "Fetch a specific module, provided a module's ID"
+    module(id: ID!): Module!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -45,6 +47,10 @@ const typeDefs = gql`
     title: String!
     "The module's length in minutes"
     length: Int
+    "The module's video URL"
+    videoUrl: String!
+    "The module's content"
+    content: String!
   }
 `;
 
